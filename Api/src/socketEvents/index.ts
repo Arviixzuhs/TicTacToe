f453751dd700077cfdb8e232613bb977bb67950b
Arviixzuh <arviixzuh@gmail.com>
2024-02-21 16:58:35 -0400

    first commit

--- Api/src/socketEvents/index.ts
+++ Api/src/socketEvents/index.ts
@@ -13,9 +13,8 @@ const handleSquare = async (io: any, data: any) => {
 
 /* Funciona para crear una nueva sala */
 const handleCreateNewGame = async (socket: any, data: any) => {
-  if (!data.name || data.name.trim() == '') {
+  if (!data.name || data.name.trim() == '')
     return socket.emit('api:roomLogInfo', { description: 'Debes ingresar tu nombre' })
-  }
 
   const room: string = 'room-' + rooms.length
   socket.join(room)
@@ -30,28 +29,22 @@ const handleCreateNewGame = async (socket: any, data: any) => {
 
 /* Funcion para entrar a una sala que ya existe */
 const handleJoinAnExistingGame = async (socket: any, data: any) => {
-  if (!data.name || data.name.trim() == '') {
+  if (!data.name || data.name.trim() == '')
     return socket.emit('api:roomLogInfo', { description: 'Debes ingresar tu nombre' })
-  }
 
-  if (!data.roomId || data.roomId.trim() == '') {
+  if (!data.roomId || data.roomId.trim() == '')
     return socket.emit('api:roomLogInfo', { description: 'Debes ingresar la id de una sala' })
-  }
 
   const room = rooms.find((item) => item.roomId == data.roomId)
+  if (!room) return socket.emit('api:roomLogInfo', { description: 'Esa sala no existe' })
 
-  if (room) {
-    if (room.users.length == 2) {
-      return socket.emit('api:roomLogInfo', { description: 'Esa sala está llena' })
-    } else {
-      socket.join(data.roomId)
-      socket.emit('api:newGame', { name: data.name, room: data.roomId, turn: 'O' })
-      socket.to(data.roomId).emit('api:startGame')
-      room.users.push(socket.id)
-    }
-  } else {
-    return socket.emit('api:roomLogInfo', { description: 'Esa sala no existe' })
-  }
+  if (room.users.length == 2)
+    return socket.emit('api:roomLogInfo', { description: 'Esa sala está llena' })
+
+  socket.join(data.roomId)
+  socket.emit('api:newGame', { name: data.name, room: data.roomId, turn: 'O' })
+  socket.to(data.roomId).emit('api:startGame')
+  room.users.push(socket.id)
 }
 
 const socketEvents = (io: any) => {
