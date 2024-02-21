110de41bdfa3644ad8e57b489510995a0d8d5331
Arviixzuh <arviixzuh@gmail.com>
2024-02-21 16:06:56 -0400

    first commit

--- Api/src/socketEvents/index.ts
+++ Api/src/socketEvents/index.ts
@@ -13,6 +13,10 @@ const handleSquare = async (io: any, data: any) => {
 
 /* Funciona para crear una nueva sala */
 const handleCreateNewGame = async (socket: any, data: any) => {
+  if (!data.name || data.name.trim() == '') {
+    return socket.emit('api:roomLogInfo', { description: 'Debes ingresar tu nombre' })
+  }
+
   const room: string = 'room-' + rooms.length
   socket.join(room)
 
@@ -28,6 +32,14 @@ const handleCreateNewGame = async (socket: any, data: any) => {
 const handleJoinAnExistingGame = async (socket: any, data: any) => {
   const room = rooms.find((item) => item.roomId == data.roomId)
 
+  if (!data.name || data.name.trim() == '') {
+    return socket.emit('api:roomLogInfo', { description: 'Debes ingresar tu nombre' })
+  }
+
+  if (!data.roomId || data.roomId.trim() == '') {
+    return socket.emit('api:roomLogInfo', { description: 'Debes ingresar la id de una sala' })
+  }
+
   if (room) {
     if (room.users.length == 2) {
       return socket.emit('api:roomLogInfo', { description: 'Esa sala está llena' })
@@ -35,7 +47,7 @@ const handleJoinAnExistingGame = async (socket: any, data: any) => {
       socket.join(data.roomId)
       socket.emit('api:newGame', { name: data.name, room: data.roomId, turn: 'O' })
       socket.to(data.roomId).emit('api:startGame')
-      room.users.push(socket.io)
+      room.users.push(socket.id)
     }
   } else {
     return socket.emit('api:roomLogInfo', { description: 'Esa sala no existe' })
